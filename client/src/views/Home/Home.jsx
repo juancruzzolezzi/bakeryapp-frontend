bc087f5fb4260fc25bc33e55bb2c2307f62d9924
Juan <jzolezzi@uade.edu.ar>
2026-08-07 19:47:52 -0300

    Auto-clear cart on Home when redirected back from a successful payment

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router-dom";
 import style from "./Home.module.css";
 import NavBarHome from "../../components/Navs/NavBarHome/NavBarHome";
+import { emptyCart } from "../../redux/slice/homeSlice";
 // import ProductsHome from "../../components/ProductsHome/ProductsHome";
 
 function Home() {
+  const dispatch = useDispatch();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  //Si volvemos de un pago aprobado, vaciamos el carrito y limpiamos la URL
+  useEffect(() => {
+    if (searchParams.get("payment") === "success") {
+      dispatch(emptyCart());
+      setSearchParams({}, { replace: true });
+    }
+  }, [searchParams, dispatch, setSearchParams]);
+
   return (
     <div className={style.mainContainer}>
       <NavBarHome />
@@ -11,7 +25,7 @@ function Home() {
         <div className={style.presentacion}>
           <h1>BakeryApp</h1>
           <h2>[baked goods & cookies]</h2>
-        </div> 
+        </div>
       </div>
       {/* <ProductsHome/> */}
     </div>
